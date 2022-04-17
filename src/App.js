eb483628b7443f7671c84c9543dc4151e590d8e5
arifvtti <arif.vtti@gmail.com>
2022-04-17 22:40:34 +0600

    banner part done

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import About from './Pages/About/About';
 import Login from './Pages/Authentication/Login/Login';
 import Blogs from './Pages/Blogs/Blogs';
+import Home from './Pages/Home/Home/Home';
 import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header></Header>
 
       <Routes>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/blogs" element={<Blogs />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/login" element={<Login />}></Route>
---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import img from "../../../img/doctor.jpg";
+const Home = () => {
+    return (
+        <div>
+
+            <div className='container d-md-flex justify-content-center align-items-center my-md-5 p-3'>
+                <div className=''>
+                    <h1 className='text-success fw-bold'>DOCTOR KHANA</h1>
+                    <p>Assalamu Alaikum, I am AB Arif. I am a doctor by profession. I'm working on cancer. I work with different types of cancer. Such as: Brain Cancer, Breast Cancer, Screen Cancer etc. I want you, me and you all to be healthy in sha Allah. I would be personally happy if I could help you or someone you know. Contact me if you or someone you know has cancer or its symptoms.</p>
+                </div>
+                <div className='px-md-5'>
+                    <img className='rounded-3 img-fluid' src={img} alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
